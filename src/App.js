a87e973083d38fbbf5be8865dfb9f4a2e918ed57
bryan <obraye05@gmail.com>
2023-05-08 09:44:17 +0100

    add Oauth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton,
+} from '@clerk/clerk-react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import Hero from "./component/Hero";
 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+function PublicPage() {
+  return (
+    <>
+      <Hero  />
+    </>
+  );
+}
+
+function ProtectedPage() {
+  return (
+    <>
+      <h1>Protected page</h1>
+      <UserButton />
+    </>
+  );
+}
+
+function ClerkProviderWithRoutes() {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+}
 
 function App() {
   return (
-    <div className="">
-     <Hero />
-    </div>
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -6,15 +6,16 @@ const Hero = () => {
       {/* Overley */}
       <div className="absolute top-0 right-0 bottom-0 left-0 bg-black/40  " />
       <div className="p-5 text-white ">
-        <h1 className="font-bold  text-5xl ">BriFlight </h1>
+        <h1 className="font-bold  text-5xl relative">BriFlight </h1>
         <p className="py-3 text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
           adipisci dolore{' '}
         </p>
-        <button className="relative border py-2 px-4 cursor-pointer z-10 hover:bg-transparent rounded-lg border-gray hover:border-blue-300  hover:text-blue-300  ">
-          {' '}
-          Sign in{' '}
-        </button>
+        <a href="/protected">
+          <button className="relative border py-2 px-4 cursor-pointer z-10 hover:bg-transparent rounded-lg border-gray hover:border-blue-300  hover:text-blue-300  ">
+            Sign in
+          </button>
+        </a>
       </div>
     </div>
   );
