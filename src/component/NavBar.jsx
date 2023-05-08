3f545cb5cadb27a3b57dc5a381bfcb20ab05ec2d
bryan <obraye05@gmail.com>
2023-05-08 09:21:12 +0100

    add Hero

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <header className="max-w-[1240px] mx-auto flex justify-between items-center">
+        <h1>logo-herr</h1>
+        <ul></ul>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
